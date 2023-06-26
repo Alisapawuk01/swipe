ac1952561e2b58276f0c3d0d775eebf7215893af
Alisapawuk01 <singi_01@mail.ru>
2023-06-26 21:06:19 +0300

    внутренняя структура уведомлений

--- app/src/main/java/com/example/swipe/MainActivity.kt
+++ app/src/main/java/com/example/swipe/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.swipe
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -32,6 +35,8 @@ class MainActivity : AppCompatActivity() {
 //           }
 //        }
 
+        createNotificationChannel()
+
         db.getDao().getAllItem().asLiveData().observe(this) { list->
             taskListAdapter.ClearTaskList()
             list.forEach {
@@ -58,6 +63,18 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("BtnClickIndex", insertedIndex + 1)
             startActivity(intent)
            }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val name: CharSequence = "pawuk channel"
+            val description = "Channel For Alarm Manager"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel( "pawuk", name, importance)
+            channel.description = description
+            val notificationManager = getSystemService(
+                NotificationManager::class.java)
+    }
     }
 
     private fun init()
---
+++ app/src/main/java/com/example/swipe/Notifications.kt
@@ -0,0 +1,38 @@
+package com.example.swipe
+
+import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class Notifications : BroadcastReceiver() {
+    @SuppressLint("MissingPermission")
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        val i =  Intent(context, ActivityPage2::class.java)
+        intent!!.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(context, 0, i, PendingIntent.FLAG_IMMUTABLE)
+
+        val builder = NotificationCompat.Builder(context!!, "pawuk")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Вещи!")
+            .setContentText("Вещи сами себя не проверят!")
+            .setAutoCancel(true)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(123, builder.build())
+
+
+
+    }
+
+
+
+}
